f5f76bcd6dfb78857d59a71b9b3b3a63e7a9a9ef
asadullo <asadullo.ganiev@finnetlimited.com>
2022-12-12 22:35:47 +0500

    Initialized a spring project

---
+++ ielts-prep-uz/src/main/java/com/example/ieltsprepuz/IeltsPrepUzApplication.java
@@ -0,0 +1,13 @@
+package com.example.ieltsprepuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeltsPrepUzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IeltsPrepUzApplication.class, args);
+    }
+
+}
---
+++ ielts-prep-uz/src/test/java/com/example/ieltsprepuz/IeltsPrepUzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ieltsprepuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IeltsPrepUzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
